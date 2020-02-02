916e177f32b3637cc4043a304f3d0ceff26a088c
ArtemSharapanyk <artmencd2003@gmail.com>
2020-02-02 21:10:47 +0200

    upgraded menu logic

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const filter     = require('./js/filters');
     const slider     = require('./js/slider');
     
-    createMenu('.menu', '.burgBtn', 'menuActive');
+    createMenu('.menu', '.burgBtn', 'menuActive', 'nav a', 'activeMenuLink', 'activeMenuOnScroll');
     filter('.filtration','.workExample','.filterBtn');
     slider('.dot', '.slide2', '.dots', '.ourPartners .slider');
 });
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,6 +1,8 @@
-let menu = (menu, btn, activeClass) => {
-    let menuWrap = document.querySelector(menu);
-    let menuBtn  = document.querySelector(btn);
+let menu = (menu, btn, activeClass, link, activeLinkClass, onScrollActive) => {
+    let menuWrap       = document.querySelector(menu);
+    let menuBtn        = document.querySelector(btn);
+    let links          = document.querySelectorAll(link);
+    let navigation     = document.querySelector('nav');
 
     menuBtn.addEventListener('click', () => {
         if(menuWrap.classList.contains(activeClass)) {
@@ -10,6 +12,38 @@ let menu = (menu, btn, activeClass) => {
         else{
             menuWrap.classList.add(activeClass);
             menuBtn.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>';
+        }
+    });
+    document.addEventListener('scroll', () => {
+        let addActiveClassToMenuItemsOrRemove = (num) => {
+            links.forEach((item) => item.classList.remove(activeLinkClass));
+            links[num].classList.add(activeLinkClass);
+        };
+
+        if(document.documentElement.scrollTop >= 100) {
+            navigation.classList.add(onScrollActive);
+        }
+        else{
+            navigation.classList.remove(onScrollActive);
+        }
+        
+        if(document.documentElement.scrollTop >= 0) {
+            addActiveClassToMenuItemsOrRemove(0);
+            if(document.documentElement.scrollTop > 572) {
+                addActiveClassToMenuItemsOrRemove(1);
+                if(document.documentElement.scrollTop > 1100) {
+                    addActiveClassToMenuItemsOrRemove(2);
+                    if(document.documentElement.scrollTop > 1802) {
+                        addActiveClassToMenuItemsOrRemove(3);
+                        if(document.documentElement.scrollTop > 2996) {
+                            addActiveClassToMenuItemsOrRemove(4);
+                            if(document.documentElement.scrollTop > 3902) {
+                                addActiveClassToMenuItemsOrRemove(5);
+                            }
+                        }
+                    }
+                }
+            }
         }
     });
 };
